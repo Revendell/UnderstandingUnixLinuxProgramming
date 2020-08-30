d79c4d4335d6571803a1fceea0c4932a27867f97
Rvendell <2312237896@qq.com>
2020-08-30 18:08:02 +0800

    Create play_again0.c

---
+++ 07 signal/play_again0.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<termios.h>
+#define QUESTION "Do you want another transaction"
+int get_response(char *question)
+{
+    printf("%s (y/n)?",question);
+    while(1)
+    {
+        switch(getchar())
+        {
+            case 'y':
+            case 'Y': return 0;
+            case 'n':
+            case 'N':
+            case EOF: return 1;
+        }
+    }
+}
+int main()
+{
+    int response;
+    response=get_response(QUESTION);
+    return response;
+}
