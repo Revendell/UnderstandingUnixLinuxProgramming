c4cb8390119b3d67dc8255b920c0fc76161d6300
Rvendell <2312237896@qq.com>
2020-09-05 01:00:33 +0800

    Create sigdemo3.c

---
+++ 10 信号处理/sigdemo3.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include<signal.h>
+#include<string.h>
+#define INPUTLEN 100
+void inthandler(int s)
+{
+    printf("Received signal %d..waiting\n",s);
+    sleep(2);
+    printf("Leaving inthandler\n");
+}
+void quithandler(int s)
+{
+    printf("Received signal %d..waiting\n",s);
+    sleep(3);
+    printf("Leaving quithandler\n");
+}
+int main()
+{
+    char input[INPUTLEN];
+    int nchars;
+    signal(SIGINT,inthandler);
+    signal(SIGQUIT,quithandler);
+    do{
+        printf("\nType a message\n");
+        nchars=read(0,input,(INPUTLEN-1));
+        if(nchars==-1)
+            perror("read returned an error");
+        else
+        {
+            input[nchars]='\n';
+            printf("You typed: %s",input);
+        }
+    }while(strncmp(input,"quit",4)!=0);
+    return 0;
+}
