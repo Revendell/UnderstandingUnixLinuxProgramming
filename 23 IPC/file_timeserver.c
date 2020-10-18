1e488aee1053ed98f6935a0b16b27fa9baa05741
Rvendell <2312237896@qq.com>
2020-10-19 01:09:33 +0800

    Create file_timeserver.c

---
+++ 23 IPC/file_timeserver.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+#include<sys/file.h>
+#include <sys/ipc.h>
+#include<time.h>
+#include<fcntl.h>
+#include<string.h>
+#include<stdlib.h>
+#define oops(m,x) {perror(m); exit(x);}
+void lock_operation(int fd,int op)
+{
+    struct flock lock;
+    lock.l_whence=SEEK_SET;
+    lock.l_start=lock.l_len=0;
+    lock.l_pid=getpid();
+    lock.l_type=op;
+    if(fcntl(fd,F_SETLKW,&lock)==-1)
+        oops("lock operation",6);
+}
+int main(int argc,char *argv[])
+{
+    int fd;
+    time_t now;
+    char *message;
+    if(argc!=2)
+    {
+        fprintf(stderr,"usage:%s filename",argv[0]);
+        exit(1);
+    }
+    if((fd=open(argv[1],O_CREAT|O_TRUNC|O_WRONLY,0644))==-1)
+        oops(argv[1],2);
+    while(1)
+    {
+        time(&now);
+        message=ctime(&now);
+        lock_operation(fd,F_WRLCK);
+        if(lseek(fd,0L,SEEK_SET)==-1)
+            oops("lseek",3);
+        if(write(fd,message,strlen(message))==-1)
+            oops("write",4);
+        lock_operation(fd,F_UNLCK);
+        sleep(1);
+    }
+    return 0;
+}
